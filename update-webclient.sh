432e419f4ec7d80a0557cdefdb2e8012a92858ac
ajkuftic <ajkuftic@gmail.com>
2016-07-22 15:09:34 -0400

    Cert check disabled

--- update-webclient.sh
+++ update-webclient.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 VER=$1    # sets version variable
 /etc/init.d/vsphere-client stop    # stops vSphere Web Client
-wget https://download3.vmware.com/software/vmw-tools/vsphere_html_client/installer-$VER.0.bsx   # downloads the client and avoids cert check
+wget https://download3.vmware.com/software/vmw-tools/vsphere_html_client/installer-$VER.0.bsx --no-check-certificates  # downloads the client and avoids cert check
 chmod +x installer-$VER.0.bsx   # sets execute permissions on installer file
 ./installer-$VER.0.bsx   # runs the installer file
 /etc/init.d/vsphere-client start  # starts the vSphere Web Client
